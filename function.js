329660f630936f6427dad30d90044cfac0dbdc9c
WateryPickleManiac <njegos.jerinic@gmail.com>
2023-09-15 11:37:13 +0200

    same as last

--- function.js
+++ function.js
@@ -1,27 +1,12 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-    var slideIndex = 1;
-    showSlides(slideIndex);
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
 
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
-    }
-
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
-
-    function showSlides(n) {
-      var i;
-      var slides = document.getElementsByClassName("mySlides");
-      var dots = document.getElementsByClassName("dot");
-      if (n > slides.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-      }
-      for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex-1].style.display = "block";
-      dots[slideIndex-1].className += " active";
-    }
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click" , () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
