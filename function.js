34b8c7c72665a20563e35bcee60a2d59040f685b
WateryPickleManiac <njegos.jerinic@gmail.com>
2023-09-17 21:50:49 +0200

    making moves

--- function.js
+++ function.js
@@ -10,3 +10,14 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click" ,
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+const nav = document.getElementById('mynav');
+window.onscroll = function () { 
+    if (document.body.scrollTop >= 200 ) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
