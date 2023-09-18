8e7f26ec4028996b5e869316061ea250e3d65ce3
WateryPickleManiac <njegos.jerinic@gmail.com>
2023-09-18 17:15:02 +0200

    monday upgrades

--- function.js
+++ function.js
@@ -10,14 +10,20 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click" ,
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
-const nav = document.getElementById('mynav');
-window.onscroll = function () { 
-    if (document.body.scrollTop >= 200 ) {
-        myNav.classList.add("nav-colored");
-        myNav.classList.remove("nav-transparent");
-    } 
-    else {
-        myNav.classList.add("nav-transparent");
-        myNav.classList.remove("nav-colored");
+const navbar = document.querySelector('.navbar');
+const navLink = document.querySelectorAll('.nav-');
+
+// Function to change navbar color on scroll
+function changeNavbarColorOnScroll() {
+    if (window.scrollY > 10) {
+        navbar.style.backgroundColor = 'white'; // Change background to white on scroll
+        navLink.style.color = 'indianred'; // Change text color to indianred on scroll
+    } else {
+        navbar.style.backgroundColor = 'transparent'; // Reset background to transparent when at the top
+        navLink.style.color = 'white'; // Reset text color to white when at the top
     }
-};
+}
+
+// Listen for the scroll event and call the function
+window.addEventListener('scroll', changeNavbarColorOnScroll);
+
