e28e3db56175f67f05774a19d30f930f0fdabe5c
WateryPickleManiac <njegos.jerinic@gmail.com>
2023-10-11 10:56:42 +0200

    needs improvement

--- function.js
+++ function.js
@@ -47,37 +47,23 @@ function changeNavbarColorOnScroll() {
 window.addEventListener('scroll', changeNavbarColorOnScroll);
 
 let slideIndex = 0;
-showSlides(slideIndex);
+showSlides();
 
-function showSlides(n) {
+function showSlides() {
     let slides = document.getElementsByClassName("slide");
-    let thumbnails = document.getElementsByClassName("thumbnail");
-    
-    if (n >= slides.length) {
-        slideIndex = 0;
-    }
-    if (n < 0) {
-        slideIndex = slides.length - 1;
-    }
 
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    
-    for (let i = 0; i < thumbnails.length; i++) {
-        thumbnails[i].style.border = "2px solid #ccc";
+
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
     }
-    
-    slides[slideIndex].style.display = "block";
-    thumbnails[slideIndex].style.border = "2px solid #f00";
+
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 2000); // Change slides every 2 seconds (2000 milliseconds)
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-// Automatically advance the slideshow
-setInterval(() => {
-    showSlides(++slideIndex);
-}, 3000);
 
