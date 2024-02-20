253b35bcff5f659c683ef1bf6111098c5733c279
Njegos Jerinic <njegos.jerinic@gmail.com>
2024-02-20 17:52:40 +0100

    WIP

--- function.js
+++ function.js
@@ -1,5 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const navMenuActive = document.querySelector(".nav-menu active");
+const navbar = document.querySelector(".navbar");
+
+
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -10,41 +14,6 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click" ,
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
-const navbar = document.querySelector('.navbar');
-const navBrand = document.querySelector('.nav-branding'); // Select the branding/logo element
-const navItems = document.querySelectorAll('.nav-item'); // Select all nav-items
-
-// Function to change navbar elements' color on scroll for larger screens
-function changeNavbarColorOnScroll() {
-    if (window.innerWidth > 768) { // Check viewport width
-        if (window.scrollY > 10) {
-            navbar.style.backgroundColor = 'white'; // Change background to white on scroll
-            navBrand.style.color = 'indianred'; // Change branding/logo color to indianred on scroll
-
-            // Loop through each nav-item and change text color to indianred
-            navItems.forEach(navItem => {
-                const navLink = navItem.querySelector('.nav-link');
-                if (navLink) {
-                    navLink.style.color = 'indianred';
-                }
-            });
-        } else {
-            navbar.style.backgroundColor = 'transparent'; // Reset background to transparent when at the top
-            navBrand.style.color = 'white'; // Reset branding/logo color to white when at the top
-
-            // Loop through each nav-item and change text color to white
-            navItems.forEach(navItem => {
-                const navLink = navItem.querySelector('.nav-link');
-                if (navLink) {
-                    navLink.style.color = 'white';
-                }
-            });
-        }
-    }
-}
-
-// Listen for the scroll event and call the function
-window.addEventListener('scroll', changeNavbarColorOnScroll);
 
 
 function toggleExpand(item) {
