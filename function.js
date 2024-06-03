afaf20a007e3fff2ba2b2b70c473995471e92720
Njegos Jerinic <njegos.jerinic@gmail.com>
2024-06-03 11:00:17 +0200

    translation to json

--- function.js
+++ function.js
@@ -32,3 +32,41 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click" ,
     }
   });
   
+  const languageFlags = {
+    en: "https://lh3.googleusercontent.com/pw/AP1GczMQ1wepx53rXXjpqptsnoiCiKUmokLVyiSV2UtJt9VNQV05Q6pVmDAzcvxS64TewnFHX1-xfzz09G_-cFrDs90YeR_oarc60KS9kxDn34AIQtX8UlmNsT44R1_NkNRWMkhNmwi_oinPFPb4dDlEa3iy=w318-h159-s-no-gm?authuser=0",
+    de: "https://lh3.googleusercontent.com/pw/AP1GczOKa3FGhAYoD0yB-qnd1zg7qk7IF2ht3W5cYaYd8hql9a1J086IM6nw1viEbI7Wkhlzibwu6pVi7xiu-shWCCHc0ONUBtPf0vsTTtPkyh0dvj3zDsYBxkpf0k-Kvb3FzLbAKAf9Pj5GjQdDw1hM7inN=w290-h174-s-no-gm?authuser=0",
+    srb: "https://lh3.googleusercontent.com/pw/AP1Gcz9-eCL_AQJgZTcUeymbVrU7S8tye-KAes0Jwbuxu8xwXrwQOwbbVnLrT6-YveLt9NL99ReXxq-5dUmiSpV5BffL2Dtls8=w300-h159-s-no-gm?authuser=0"
+};
+
+
+
+function changeLanguage(language) {
+  fetch(`translations/${language}.json`)
+      .then(response => response.json())
+      .then(data => {
+          document.querySelectorAll('[data-key]').forEach(element => {
+              const key = element.getAttribute('data-key');
+              element.textContent = data[key];
+          });
+          localStorage.setItem('selectedLanguage', language);
+          updateActiveButton(language);
+      })
+      .catch(error => console.error('Error loading translation:', error));
+}
+
+function updateActiveButton(language) {
+  document.querySelectorAll('.language-switcher button').forEach(button => {
+      button.classList.toggle('active', button.getAttribute('onclick').includes(language));
+  });
+}
+
+function toggleLanguage(language) {
+  changeLanguage(language);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedLanguage = localStorage.getItem('selectedLanguage') || 'en';
+  changeLanguage(savedLanguage);
+});
+
+
